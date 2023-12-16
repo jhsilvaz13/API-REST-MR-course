42350938bd917d3f88207af3b729f520c06fd928
jhsilvaz13 <jhsilvaz@unal.edu.co>
2023-12-16 18:24:11 -0500

    Router session commented

--- red-bicicletas/app.js
+++ red-bicicletas/app.js
@@ -52,7 +52,7 @@ app.use("/token", tokenRouter);
 //app.use("/bicicletas", loggedIn, bicicletasRouter);
 app.use("/api/bicicletas", validarUsuario, bicicletasAPIRouter);
 app.use("/api/usuarios", validarUsuario, usuariosAPIRouter);
-app.use("/session", sessionRouter);
+//app.use("/session", sessionRouter);
 app.use("/api/auth", verificado, authAPIRouter);
 
 // catch 404 and forward to error handler
